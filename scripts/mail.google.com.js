3845d6fdd3ac2419bab0081331af0cfb9c6c9d4f
Emil Telstad <22004178+emilte@users.noreply.github.com>
2024-09-20 19:37:31 +0200

    Use events instead.

--- scripts/mail.google.com.js
+++ scripts/mail.google.com.js
@@ -1,11 +1,17 @@
-import { INTERVAL_MS, MARKER, TRUSTED_DOMAINS } from '../utils/constants.js';
+import { TRUSTED_DOMAINS } from '../utils/constants.js';
 import { getDomain, makeDomainElement, mark } from '../utils/helpers.js';
 
 function main() {
 	const mailBody = document.querySelector('[data-message-id]');
 	if (!mailBody) return;
 
-	const links = mailBody.querySelectorAll(`a:not([${MARKER}])`);
+	/**
+	 * Select all links with href.
+	 * After being parsed, the href will be removed and excluded from future queries.
+	 */
+	const links = mailBody.querySelectorAll('a[href]');
+	// const links = mailBody.querySelectorAll(`a:not([${MARKER}])`);
+
 	for (const link of links) {
 		link.title = link.href; // Show full url on hover.
 
@@ -28,5 +34,8 @@ function main() {
 	}
 }
 
-// Run main regularly. (Maybe use eventListener?)
-setInterval(main, INTERVAL_MS);
+// Run main regularly.
+// setInterval(main, INTERVAL_MS);
+setTimeout(main, 2000);
+window.addEventListener('popstate', () => setTimeout(main, 300));
+window.addEventListener('hashchange', () => setTimeout(main, 300));
